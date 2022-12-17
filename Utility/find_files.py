b54a694eca1a3a40477eba9d9b25332d6c185599
Magda Wójcicka <magda.wojcicka.19.09@gmail.com>
2022-12-17 16:12:34 +0100

    add directory as parameter to find_files function

--- Utility/find_files.py
+++ Utility/find_files.py
@@ -4,10 +4,10 @@
 import os
 
 
-def get_files_with_extension(extension='.ipynb'):
+def get_files_with_extension(extension='.ipynb',directory='.'):
     """Get list of paths for files with given extension """
     files_list = []
-    for root, _, files in os.walk('.'):
+    for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith(extension):
                 fullpath = os.path.join(root, file)
